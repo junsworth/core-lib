a52388d56bfb771c329838d74f61cf96c0592c2c
Jonathan Unsworth <jonathan@bubbleworks.co.za>
2017-02-12 15:21:16 +0200

    Initial commit...

---
+++ CoreLib/CoreLib.h
@@ -0,0 +1,19 @@
+//
+//  CoreLib.h
+//  CoreLib
+//
+//  Created by Jonathan Unsworth on 2017/02/12.
+//  Copyright © 2017 Jonathan Unsworth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CoreLib.
+FOUNDATION_EXPORT double CoreLibVersionNumber;
+
+//! Project version string for CoreLib.
+FOUNDATION_EXPORT const unsigned char CoreLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreLib/PublicHeader.h>
+
+
---
+++ CoreLib/Models/User.swift
@@ -0,0 +1,30 @@
+//
+//  User.swift
+//  CoreLib
+//
+//  Created by Jonathan Unsworth on 2017/02/12.
+//  Copyright © 2017 Jonathan Unsworth. All rights reserved.
+//
+
+import Foundation
+
+class User {
+    // MARK properties
+    var firstName: String
+    var lastName: String
+    var photo: UIImage?
+    
+    
+    // MARK: Initialization
+    init?(firstName: String, lastName: String, photo: UIImage?) {
+        // Initialize stored properties.
+        self.firstName = firstName
+        self.lastName = lastName
+        self.photo = photo
+        
+        // Initialization should fail if there is no name or if the rating is negative.
+        if firstName.isEmpty || lastName.isEmpty  {
+            return nil
+        }
+    }
+}
