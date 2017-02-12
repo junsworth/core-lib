d3bdf6598928d9e46f990ad2541960c921174285
Jonathan Unsworth <jonathan@protonmail.com>
2017-02-12 21:40:04 +0200

    Added ‘AlamofireServices’ singleton

---
+++ CoreLib/src/Services/AlamofireServices.swift
@@ -0,0 +1,35 @@
+//
+//  AlamofireServices.swift
+//  CoreLib
+//
+//  Created by Jonathan Unsworth on 2017/02/12.
+//  Copyright © 2017 Jonathan Unsworth. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class AlamoreServices {
+    
+    static let sharedInstance = AlamoreServices()
+    
+    func getURLRequest(url:String) -> String {
+        
+        var result:String = ""
+        
+        Alamofire.request("https://httpbin.org/get").responseJSON { response in
+            print(response.request!)  // original URL request
+            print(response.response!) // HTTP URL response
+            print(response.data!)     // server data
+            print(response.result)   // result of response serialization
+            if let JSON = response.result.value {
+                print("JSON: \(JSON)")
+                result = "\(JSON)"
+            }
+        }
+        
+        return result
+        
+    }
+    
+}
