1465e1c59455fae505ea395f8046b51625e0b36b
Jonathan Unsworth <jonathan@protonmail.com>
2017-02-22 07:22:34 +0200

    1. Add ModelProtocol

---
+++ CoreLib/src/Models/Model.swift
@@ -0,0 +1,14 @@
+//
+//  Model.swift
+//  CoreLib
+//
+//  Created by Jonathan Unsworth on 2017/02/21.
+//  Copyright © 2017 Jonathan Unsworth. All rights reserved.
+//
+
+import Foundation
+
+// MARK: Model Protocol
+protocol ModelProtocol {
+    
+}
