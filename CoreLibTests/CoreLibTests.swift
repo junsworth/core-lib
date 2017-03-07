5a4716825b89436b8b29ddf9dda44c8df9b191e1
jonathanu <jonathanu@globalkinetic.com>
2017-03-07 10:56:49 +0200

    Modify unit tests

--- CoreLibTests/CoreLibTests.swift
+++ CoreLibTests/CoreLibTests.swift
@@ -7,7 +7,12 @@
 //
 
 import XCTest
+
 @testable import CoreLib
+
+import SwiftyJSON
+import Quick
+import Nimble
 
 class CoreLibTests: XCTestCase {
     
@@ -33,22 +38,72 @@ class CoreLibTests: XCTestCase {
         }
     }
     
+    // Confirm user model api
+    func testUserModelAPI() {
+        self.measure {
+            
+            // Mock model
+            var user: User = UserMockAPI().inject() as! User
+            
+            // Assert mock model exists
+            XCTAssertNotNil(user)
+            
+            // Assert user id > 0
+            XCTAssertTrue(user.id>0)
+            user.firstName = "Hello"
+            // Assert user first name
+            XCTAssertFalse(user.firstName.isEmpty)
+            
+            // Expect first name
+            expect(user.firstName).to(equal("Jonathan"))
+            
+            // Assert user last name
+            XCTAssertFalse(user.lastName.isEmpty)
+            
+            // Expect first name
+            expect(user.lastName).to(equal("Unsworth"))
+            
+            let users: [User] = UserMockAPI().injectArray() as! [User]
+            
+            expect(users.count).to(equal(2))
+        }
+    }
+    
+    // Confirm user model api
+    func testDeviceModelAPI() {
+        self.measure {
+            let device: Device = DeviceMockAPI().inject() as! Device
+            
+            expect(device.getName()).to(equal("Jono's Device"))
+        }
+    }
+    
     // Confirm the User initializer works
     func testUserInitializersSucceed(){
         
         self.measure {
         
             // Mock user
-            let mockUser = try! User.init(json: Utils.mock(mock: "user"))
+            let mockUser = UserMockAPI().inject() as! User
             
             // Assert mock user exists
             XCTAssertNotNil(mockUser)
             
             // Assert user's id > 0
-            XCTAssertTrue(mockUser!.id>0)
+            XCTAssertTrue(mockUser.id>0)
             
             // Assert user name
-            XCTAssertFalse(mockUser!.firstName.isEmpty)
+            XCTAssertFalse(mockUser.firstName.isEmpty)
+            
+            let jsonUser = UserMockAPI().inject() as! User
+            
+            XCTAssertNotNil(jsonUser)
+            
+            // Assert user's id > 0
+            XCTAssertTrue(jsonUser.id>0)
+            
+            // Assert user name
+            XCTAssertFalse(jsonUser.firstName.isEmpty)
             
             let namedUser = User.init(id: 1, firstName: "Jonathan", lastName: "Unsworth")
             XCTAssertNotNil(namedUser)
@@ -58,9 +113,13 @@ class CoreLibTests: XCTestCase {
             
             XCTAssertFalse(namedUser==unNamedUser)
             
-            let mockUsers: [User] = User.users(json: Utils.mock(mock: "users"), key: "users")
+            let mockUsers: [User] = User.users(json: Utils.inject(model: "users", bundleIdentifier: Keyword.coreLibBundleIdentifier), key: "users")
             
             XCTAssertTrue(mockUsers.count>0)
+            
+            let swiftyUsers: [User] = User.users(json: JSON(data: Utils.dataForResource(bundleIdentifier: Keyword.coreLibBundleIdentifier, withGKResource: "users", withGKExtension: Keyword.jsonFileExt)), key: "users")
+            
+            XCTAssertTrue(swiftyUsers.count>0)
             
         }
         
