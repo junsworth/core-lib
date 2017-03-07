d345f373d34092084ab033c522f95b7b1fc828d5
jonathanu <jonathanu@globalkinetic.com>
2017-03-07 10:54:18 +0200

    Add Base Request & Response

---
+++ CoreLib/src/Models/request/BaseRequest.swift
@@ -0,0 +1,20 @@
+//
+//  BaseRequest.swift
+//  CoreLib
+//
+//  Created by Jonathan Unsworth on 2017/03/03.
+//  Copyright © 2017 Jonathan Unsworth. All rights reserved.
+//
+
+import Foundation
+
+// MARK: BaseRequst protocol
+protocol BaseRequest {
+    var device: Device { get set }
+    init()
+}
+
+// MARK: 
+extension BaseRequest {
+    
+}
---
+++ CoreLib/src/Models/response/BaseResponse.swift
@@ -0,0 +1,13 @@
+//
+//  BaseResponse.swift
+//  CoreLib
+//
+//  Created by Jonathan Unsworth on 2017/03/03.
+//  Copyright © 2017 Jonathan Unsworth. All rights reserved.
+//
+
+import Foundation
+
+protocol BaseResponse {
+    
+}
